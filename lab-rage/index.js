f0ff0ac145ca020714a6510549d14714006ee4a4
Abigail White <abigailwhite213@gmail.com>
2017-04-21 13:01:27 -0700

    all transforms functional

--- lab-rage/index.js
+++ lab-rage/index.js
@@ -17,4 +17,4 @@ module.exports = exports = function() {
   });
 };
 
-exports()
+exports();
--- lab-rage/lib/readFile.js
+++ lab-rage/lib/readFile.js
@@ -5,6 +5,6 @@ module.exports = function (input, callback) {
   fs.readFile(input, (err, data) => {
     if(err) throw err;
     let imgData = new Bitmap(data);
-    callback(null, imgData);
+    callback(null, imgData); // look up null functionality
   });
 };
