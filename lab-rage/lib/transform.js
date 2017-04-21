3eebe1b5526a31f3d2c989e207ec5a28c9fc89ac
DavidGWheeler <wots_uh_thedeal@msn.com>
2017-04-21 09:59:54 -0700

    add changes to bmpSkuMaker.js

--- lab-rage/lib/transform.js
+++ lab-rage/lib/transform.js
@@ -1,5 +1,7 @@
 'use strict';
 
+module.exports = exports = {};
+
 //function to invert colors
 let invertColors = function (bitmap) {
   for (var i = 0; i < bitmap; i++) {
@@ -12,7 +14,21 @@ let invertColors = function (bitmap) {
 
 
 //function to greyscale colors
+exports.monoChrome = (buff, err) => {
+  if(err) throw new Error('Failed to change image to black and white');
+  let image = new exports.MonoChrome(buff);
+  image.monoChrome(buff);
+};
 
+exports.MonoChrome = buff => {
+  this.fileSize = buff.readUInt32LE(2);
+  this.offset = buff.readUInt32LE(10);
+  this.colorArray = buff.slice(54,1078);
+};
+
+exports.GrayScale.prototype.monoChrome = () => {
+  
+};
 
 
 
