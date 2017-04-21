3559c757849df470c50bece5e5a3b89d7d7c6fd9
michaeltreat <michael.leon.treat@gmail.com>
2017-04-20 17:11:29 -0700

    Got fingerprint header and size

--- bitpop/lib/bmp-headers.js
+++ bitpop/lib/bmp-headers.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const fs = require('fs');
+
+module.exports = exports = {};
+
+exports.read = function(){
+  fs.readFile(`${__dirname}/../../assets/finger-print.bmp`, function(err, data){
+    if(err) throw err;
+    let fingerData = data;
+    exports.bmp = {};
+    exports.bmp.header = fingerData.toString('utf-8', 0,2);
+    exports.bmp.size = fingerData.readUInt32LE(2);
+    console.log(exports.bmp);
+  });
+};
+exports.read();
