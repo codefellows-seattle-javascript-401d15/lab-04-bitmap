b11e03d96b67a6ccd24ff08212ec72d64d6155a4
galang pham <phamgalang@gmail.com>
2017-04-21 13:23:38 -0700

    added command line function

--- lab-rage/index.js
+++ lab-rage/index.js
@@ -7,14 +7,23 @@ const exportFile = require('./lib/export.js');
 module.exports = exports = function() {
   return readFile('./assets/bitmap.bmp', function(err, bitmap) {
     console.log(`bitmap = ${bitmap}`);
-    if(err) throw new Error('failed to read the bitmap');
-    // transformFile.invertColors(bitmap);
-    // transformFile.monoChrome(bitmap);
-    transformFile.rgbColors(bitmap);
+    if (err) throw new Error('failed to read the bitmap');
+    if (colorPicker == 'invertcolors') {
+      transformFile.invertColors(bitmap);
+    } else if (colorPicker == 'monochrome') {
+      transformFile.monoChrome(bitmap);
+    } else if (colorPicker == 'randomcolors') {
+      transformFile.randomColors(bitmap);
+    }
     return exportFile('./assets/output.bmp', bitmap, function(err) {
-      if(err) throw new Error('failed to transform the image');
+      if (err) throw new Error('failed to transform the image');
     });
   });
 };
+
+
+let colorPicker = String(process.argv[2]).toLowerCase();
+
+
 
 exports();
