cce14f17285f078ecec3708bd8b193a480502703
michaeltreat <michael.leon.treat@gmail.com>
2017-04-20 17:58:14 -0700

    Got all the data from the bitmap image, next we are trying to convert the color array into rbg values so we can manipulate it.

--- bitpop/lib/bmp-headers.js
+++ bitpop/lib/bmp-headers.js
@@ -11,7 +11,12 @@ exports.read = function(){
     exports.bmp = {};
     exports.bmp.header = fingerData.toString('utf-8', 0,2);
     exports.bmp.size = fingerData.readUInt32LE(2);
+    exports.bmp.offset = fingerData.readUInt32LE(10);
+    exports.bmp.width = fingerData.readUInt32LE(18);
+    exports.bmp.height = fingerData.readUInt32LE(22);
+    exports.bmp.colors = fingerData.slice(54, exports.bmp.offset);
     console.log(exports.bmp);
   });
 };
 exports.read();
+// 46 or 54 for color starting point
