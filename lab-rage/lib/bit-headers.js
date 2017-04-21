c8bb6ef66d9cc57d7da65580e4c4d762c365cde2
DavidGWheeler <wots_uh_thedeal@msn.com>
2017-04-21 12:27:55 -0700

    add working monoChrome func

--- lab-rage/lib/bit-headers.js
+++ lab-rage/lib/bit-headers.js
@@ -1,16 +1,6 @@
 'use strict';
 
 const fs = require('fs');
-const bmpData = {};
-
-function getHeaderData () {
-  bmpData.spec = bitmap.toString('utf-8', 0, 2);
-  bmpData.size = bitmap.readUInt32LE(2);
-  bmpData.offset = bitmap.readUInt32LE(10);
-  bmpData.width = bitmap.readUInt32LE(18);
-  bmpData.height = bitmap.readUInt32LE(22);
-  bmpData.colorArray = bitmap.slice(54, bmpData.offset);
-  console.log(bmpData);
 
 module.exports = function(bmpImage) {
   this.bmpImage = bmpImage;
@@ -21,4 +11,4 @@ module.exports = function(bmpImage) {
   this.height = bmpImage.readUInt32LE(22);
   this.colorArray = bmpImage.slice(54, this.offset);
   console.log(this);
-}
+};
--- lab-rage/lib/transform.js
+++ lab-rage/lib/transform.js
@@ -1,15 +1,5 @@
 'use strict';
 
-module.exports = exports = {};
-
-// TODO: function to invert colors
-// let invertColors = bitmap => {
-//   for (var i = 0; i < bitmap; i++) {
-//     bitmap[i] = 255 - bitmap[i];
-//     console.log(bitmap[i]);
-//   }
-//   return bitmap;
-// };
 
 const fs = require('fs');
 const bitmap = fs.readFileSync('./assets/bitmap.bmp'); //NOT SYNCH WE WANT ASYNCH
@@ -26,33 +16,22 @@ function invertColors(bitmap) {
 }
 
 
-module.exports.invertColors = invertColors;
-
 
 //function to greyscale colors
-exports.monoChrome = (buff, err) => {
+function monoChrome(bitmap, err) {
   if(err) throw new Error('Failed to change image to black and white');
-  let image = new exports.MonoChrome(buff);
-  image.monoChrome(buff);
-};
 
-exports.MonoChrome = buff => {
-  this.fileSize = buff.readUInt32LE(2);
-  this.offset = buff.readUInt32LE(10);
-  this.colorArray = buff.slice(54,1078);
-};
-
-exports.MonoChrome.prototype.monoChrome = () => {
   let currentColor = [];
   for (var i = 0; i < 1024; i += 4) {
-    currentColor = this.colorArray.slice(i, i + 4);
+    currentColor = bitmap.colorArray.slice(i, i + 4);
     let currentColorAverage = ((currentColor[0] + currentColor[1] + currentColor[2])/3);
     currentColor[0] = currentColorAverage;
     currentColor[1] = currentColorAverage;
     currentColor[2] = currentColorAverage;
     currentColor[3] = 0;
   }
-};
+
+}
 
 // TODO: function to RGB colors
 
@@ -61,3 +40,4 @@ exports.MonoChrome.prototype.monoChrome = () => {
 
 //export functions
 module.exports.invertColors = invertColors;
+module.exports.monoChrome = monoChrome;
