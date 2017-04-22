c8fbac3a6080ae9adf467bf6090fcfcf93b2257e
Kayla Asay <kdasay16@gmail.com>
2017-04-21 17:00:57 -0700

    reconstruct

--- lab-colorKillers/index.js
+++ lab-colorKillers/index.js
@@ -1,1 +1,5 @@
 'use strict';
+
+const readFile = require();
+const writeFile = require('../lib/write-file.js');
+const transformFile = require();
---
+++ lab-colorKillers/lib/write-file.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const fs = require('fs');
+const trans = require('../lib/transform.js');
+
+
+// let newColorArray = [];
+
+fs.writeFile(, 'hex');
