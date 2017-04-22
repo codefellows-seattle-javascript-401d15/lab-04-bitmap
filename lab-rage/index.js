99a6b695d49e7e94ae24c2b3304138e957023b8c
galang pham <phamgalang@gmail.com>
2017-04-22 09:57:22 -0700

    added select image

--- lab-rage/index.js
+++ lab-rage/index.js
@@ -4,23 +4,30 @@ const readFile = require('./lib/readFile.js');
 const transformFile = require('./lib/transform.js');
 const exportFile = require('./lib/export.js');
 
-let colorPicker = String(process.argv[2]).toLowerCase();
+let colorInput = String(process.argv[2]).toLowerCase();
 let outputName = String(process.argv[3]);
+let bitmapInput = String(process.argv[4]).toLowerCase();
+let selectedBitmap = selectPath(bitmapInput);
+
+function selectPath (query) {
+  let pathArray = ['./assets/bitmap.bmp', './assets/finger.bmp', './assets/house.bmp', './npb.bmp'];
+  return pathArray.filter(el => el.indexOf(query) > -1).toString();
+}
 
 module.exports = exports = function() {
-  return readFile('./assets/bitmap.bmp', function(err, bitmap) {
+  return readFile(selectedBitmap, function(err, bitmap) {
     console.log(`bitmap = ${bitmap}`);
     if (err) throw new Error('failed to read the bitmap');
-    if (colorPicker == 'invertcolors') {
+    if (colorInput == 'invertcolors') {
       transformFile.invertColors(bitmap);
-    } else if (colorPicker == 'monochrome') {
+    } else if (colorInput == 'monochrome') {
       transformFile.monoChrome(bitmap);
-    } else if (colorPicker == 'randomcolors') {
+    } else if (colorInput == 'randomcolors') {
       transformFile.randomColors(bitmap);
     } else {
       console.log('You didn\'t pick a color you dummy');
     }
-    return exportFile(`output/new_${colorPicker}_${outputName}.bmp`, bitmap, function(err) {
+    return exportFile(`output/new_${colorInput}_${outputName}.bmp`, bitmap, function(err) {
       if (err) throw new Error('failed to transform the image');
     });
   });
