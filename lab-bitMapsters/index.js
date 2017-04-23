466578fafcf7d9f97d41867585f0366932f97810
Caleb <caleb.wells1@gmail.com>
2017-04-22 22:38:27 -0700

    init

--- lab-bitMapsters/index.js
+++ lab-bitMapsters/index.js
@@ -1,78 +1,64 @@
 'use strict';
 
+// exports = exports = {};
+
 const fs = require('fs');
-// const bitmap = fs.readFileSync(`${__dirname}/data/bitmap.bmp`);
 const bitmap = `${__dirname}/assets/bitmap.bmp`;
+const transforms = require(`${__dirname}/lib/transform.js`);
+const bitHeaders = require(`${__dirname}/lib/bit-headers.js`);
 
-// const bmp = {};
+// function Bmp(data, name){
+//   this.name = name;
+//   this.spec = data.toString('utf-8', 0, 2);
+//   this.size = data.readUInt32LE(2);
+//   this.width = data.readUInt32LE(18);
+//   this.height = data.readUInt32LE(22);
+//   this.offset = data.readUInt32LE(10);
+//   this.colorArray = data.slice(54, this.offset);
+//   this.filePath = `${__dirname}/assets/${this.name}.bmp`;
+//   this.buff = data;
+// }
+
+// function inverter(imgObject){
+//   for(let i = 0; i < imgObject.colorArray.length; i++) {
+//     let newColors = 255 - imgObject.colorArray[i].toString(16);
+//     imgObject.colorArray.writeUIntLE(newColors, i, 1);
+//   }
+// }
 //
-// bmp.spec = bitmap.toString('utf-8', 0, 2);
-// bmp.size = bitmap.readUInt32LE(2);
-// bmp.width = bitmap.readUInt32LE(18);
-// bmp.height = bitmap.readUInt32LE(22);
-// bmp.offset = bitmap.readUInt32LE(10);
-// bmp.colorArray = bitmap.slice(54, bmp.offset);
-// bmp.colorTable = bitmap.slice(41, 44);
+// function greyScale (imgObject) {
+//   for (let i = 0; i < imgObject.colorArray.length; i+=4) {
+//     let greyScale = imgObject.colorArray[i].toString(16);
+//     imgObject.colorArray.writeUIntLE(greyScale, i, 1);
+//     imgObject.colorArray.writeUIntLE(greyScale, i+1, 1);
+//     imgObject.colorArray.writeUIntLE(greyScale, i+2, 1);
+//   }
+// }
+//
+// function rgbColor (imgObject) {
+//   for (let i = 0; i < imgObject.colorArray.length; i += 4) {
+//     let rgbColor = imgObject.colorArray[i].toString(16);
+//     imgObject.colorArray.writeUIntLE(rgbColor, i, 1);
+//   }
+// }
 
-// console.log(bmp);
-// console.log(bmp.colorArray);
-
-function Bmp(data, name){
-  this.name = name;
-  this.spec = data.toString('utf-8', 0, 2);
-  this.size = data.readUInt32LE(2);
-  this.width = data.readUInt32LE(18);
-  this.height = data.readUInt32LE(22);
-  this.offset = data.readUInt32LE(10);
-  this.colorArray = data.slice(54, this.offset);
-  this.filePath = `${__dirname}/assets/${this.name}.bmp`;
-  this.buff = data;
-  //this.newColorArray = [];
-  // bmp.colorTable = bitmap.slice(41, 44);
-}
-
-function inverter(imgObject){
-  for(let i = 0; i < imgObject.colorArray.length; i++) {
-    let newColors = 255 - imgObject.colorArray[i].toString(16);
-    imgObject.colorArray.writeUIntLE(newColors, i, 1);
-    //imgObject.newColorArray.push(newColors);
-  }
-}
-
-function greyScale (imgObject) {
-  for (let i = 0; i < imgObject.colorArray.length; i+=4) {
-    let greyScale = imgObject.colorArray[i].toString(16);
-    imgObject.colorArray.writeUIntLE(greyScale, i, 1);
-    imgObject.colorArray.writeUIntLE(greyScale, i+1, 1);
-    imgObject.colorArray.writeUIntLE(greyScale, i+2, 1);
-  }
-}
-
-function rgbColor (imgObject) {
-  for (let i = 0; i < imgObject.colorArray.length; i += 4) {
-    let rgbColor = imgObject.colorArray[i].toString(16);
-    imgObject.colorArray.writeUIntLE(rgbColor, i, 1);
-  }
-}
-
-fs.readFile(bitmap, function(err, data){
-  if(err) throw err;
-  console.log(data);
-
-  var newPic = new Bmp(data, 'newpicpls');
-  // let newData = data.toString('ascii');
-  console.log(newPic);
-  // inverter(newPic);
-  // greyScale(newPic);
-  rgbColor(newPic);
-  console.log(newPic);
-
-  // console.log(newPic.newColorArray);
-
-  fs.writeFile(newPic.filePath, newPic.buff, function(err){
+exports.readWrite =
+  fs.readFile(bitmap, function(err, data){
     if(err) throw err;
+    console.log(data);
+
+    bitHeaders.newPic = new bitHeaders.Bmp(data, 'newpicpls');
+    console.log(bitHeaders.newPic);
+    // inverter(newPic);
+    // greyScale(newPic);
+    transforms.rgbColor(bitHeaders.newPic);
+    console.log(bitHeaders.newPic);
+    console.log(bitHeaders.newPic.name);
+    fs.writeFile(bitHeaders.newPic.filePath, bitHeaders.newPic.buff, function(err){
+      if(err) throw err;
+    });
   });
-});
+
 
 //proper scaffolding
 // repo
--- lab-bitMapsters/lib/transform.js
+++ lab-bitMapsters/lib/transform.js
@@ -1,1 +1,26 @@
 'use strict';
+
+module.exports = exports = {};
+
+exports.inverter = function(imgObject){
+  for(let i = 0; i < imgObject.colorArray.length; i++) {
+    let newColors = 255 - imgObject.colorArray[i].toString(16);
+    imgObject.colorArray.writeUIntLE(newColors, i, 1);
+  }
+};
+
+exports.greyScale = function(imgObject) {
+  for (let i = 0; i < imgObject.colorArray.length; i+=4) {
+    let greyScale = imgObject.colorArray[i].toString(16);
+    imgObject.colorArray.writeUIntLE(greyScale, i, 1);
+    imgObject.colorArray.writeUIntLE(greyScale, i+1, 1);
+    imgObject.colorArray.writeUIntLE(greyScale, i+2, 1);
+  }
+};
+
+exports.rgbColor = function(imgObject) {
+  for (let i = 0; i < imgObject.colorArray.length; i += 4) {
+    let rgbColor = imgObject.colorArray[i].toString(16);
+    imgObject.colorArray.writeUIntLE(rgbColor, i, 1);
+  }
+};
