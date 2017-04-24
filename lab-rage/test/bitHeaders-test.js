a5a521b80d53763d7edae7350bd3da2b8d89e0ed
DavidGWheeler <wots_uh_thedeal@msn.com>
2017-04-24 08:45:52 -0700

    add some tests, not working yet

---
+++ lab-rage/test/bitHeaders-test.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const bitHeaders = require('../lib/bitHeaders.js');
+const expect = require('chai').expect;
+
+// describe('bitHeaders', function() {
+//   describe('#bitHeaders.bmpImage', function() {
+//     expect(bitHeaders).to.be.a('function');
+//     expect(bitHeaders.spec).to.be.a('string');
+//     expect(bitHeaders.size).to.be.a('number');
+//     expect(bitHeaders.size).to.have.lengthOf(4);
+//
+//   });
+// });
