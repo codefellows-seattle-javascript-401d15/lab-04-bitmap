ed88fe3add87d98a599c3ce97c23742ed2d03635
Benjamin <ayzenbergb@gmail.com>
2017-04-24 11:10:37 -0700

    yay

--- bitpop/index.js
+++ bitpop/index.js
@@ -2,4 +2,8 @@
 
 const header = require('./lib/bmp-headers.js');
 
-header.read();
+let invert = false;
+let gray = true;
+let scale = false;
+let callback = (data) => console.log(data);
+header.read(invert, gray, scale, callback);
