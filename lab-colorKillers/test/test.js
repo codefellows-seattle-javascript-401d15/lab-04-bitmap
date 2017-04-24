8bf51c16670b89c793dc1846bde10e35f83266c9
Patrick Sheridan <sheridp2@gmail.com>
2017-04-24 11:23:38 -0700

    test 2 working

--- lab-colorKillers/test/test.js
+++ lab-colorKillers/test/test.js
@@ -16,3 +16,16 @@ describe('constructor function', function(){
     } );
   });
 });
+
+describe('invert colors function', function(){
+  describe('invert colors in colorArray', function(){
+    let testData = new constructFunc(testFile);
+    testData.invert();
+    it('should equal inverted number', function(){
+      expect(testData.colorArray[0]).to.equal(255);
+      expect(testData.colorArray[1]).to.equal(255);
+      expect(testData.colorArray[2]).to.equal(255);
+      expect(testData.colorArray[3]).to.equal(0);
+    });
+  });
+});
