60cf2c9fbe6404b56233e09c32a0a4cd75d1b0c5
Kayla Asay <kdasay16@gmail.com>
2017-04-24 11:08:11 -0700

    output

--- lab-colorKillers/index.js
+++ lab-colorKillers/index.js
@@ -3,7 +3,8 @@
 const writeFile = require('./lib/write-file.js');
 const readFile = require('./lib/bmp-reader.js');
 
-const pics = ['data/bitmap.bmp', 'data/finger-print.bmp', 'data/house.bmp', 'data/non-palette-bitmap.bmp'];
+const pics = ['data/bitmap.bmp'];
+// 'data/finger-print.bmp', 'data/house.bmp', 'data/non-palette-bitmap.bmp'];
 
 
 for (var i = 0; i < pics.length; i++) {
@@ -20,6 +21,8 @@ for (var i = 0; i < pics.length; i++) {
 
   readFile(pics[i], (err, data) => {
     if (err) throw err;
+    data.changeColor();
+
     let radNum = Math.floor(Math.random()*1000);
     writeFile('./output/file'+ radNum +'.bmp', data, (err) => {
       if(err) throw err;
@@ -28,7 +31,6 @@ for (var i = 0; i < pics.length; i++) {
 
   readFile(pics[i], (err, data) => {
     if (err) throw err;
-
     data.greyScale();
 
     let radNum = Math.floor(Math.random()*1000);
