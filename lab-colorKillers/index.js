55bab5a31fdebb0850b6616b82eb9f9ae03c0cd3
Patrick Sheridan <sheridp2@gmail.com>
2017-04-24 09:38:46 -0700

    gitignore, eslintrc added

--- lab-colorKillers/index.js
+++ lab-colorKillers/index.js
@@ -4,10 +4,9 @@ const writeFile = require('./lib/write-file.js');
 const readFile = require('./lib/bmp-reader.js');
 
 const pics = ['data/bitmap.bmp', 'data/finger-print.bmp', 'data/house.bmp', 'data/non-palette-bitmap.bmp'];
-// const newPics = ['./output/bitmap1', './output/bitmap2', './output/bitmap3', './output/finger1', './output/finger2', 'output/finger3', 'output/house1', 'output/house2', 'output/house3', 'output/non1', 'output/non2', 'output/non3'];
+
 
 for (var i = 0; i < pics.length; i++) {
-  // for (var k = 0; k < newPics.length; k++) {
 
   readFile(pics[i], (err, data) => {
     if (err) throw err;
@@ -39,4 +38,3 @@ for (var i = 0; i < pics.length; i++) {
     });
   });
 }
-// }
