763b42069d987c25b531d3d121c86a2bfc9515c9
Patrick Sheridan <sheridp2@gmail.com>
2017-04-24 10:51:26 -0700

    test const working

--- lab-colorKillers/test/test.js
+++ lab-colorKillers/test/test.js
@@ -1,21 +1,18 @@
 'use strict';
 
+const fs = require('fs');
 const writeFile = require('../lib/write-file.js');
 const readFile = require('../lib/bmp-reader.js');
 const constructFunc = require('../lib/construct.js');
+const testFile = fs.readFileSync('./data/bitmap.bmp');
 
 const expect = require('chai').expect;
-const fs = require('fs');
 
 describe('constructor function', function(){
   describe('produces an object', function(){
-    let testData = new constructFunc.BitMap(fs.readFileSync('../data/bitmap.bmp'));
-    it('should return object', function(err){
-      if(err) throw err;
-      it('should create new bitmap object', function(){
-        expect(testData).to.be.an('object');
-
-      } );
-    });
+    let testData = new constructFunc(testFile);
+    it('should create new BitMap object', function(){
+      expect(testData).to.be.an('object');
+    } );
   });
 });
