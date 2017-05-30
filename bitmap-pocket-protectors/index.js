ebd76d51b840b83bd1185f53574fb67bb0d60f74
meklr <mariareed@humanoid.net>
2017-05-29 22:29:01 -0700

    stretch: able to process non-palette bms

--- bitmap-pocket-protectors/index.js
+++ bitmap-pocket-protectors/index.js
@@ -3,5 +3,16 @@
 const fileHandler = require('./lib/file-handler.js');
 const bitmapBuilder = require('./lib/bitmap.js');
 
-// Invoke readFromFile here
-fileHandler.readFromFile(process.argv[2], process.argv[3], bitmapBuilder);
+//fileHandler.readFromFile(process.argv[2], process.argv[3], bitmapBuilder);
+
+//fileHandler.readFromFile('non-palette-bitmap.bmp', 'invert', bitmapBuilder);
+// fileHandler.readFromFile('non-palette-bitmap.bmp', 'grayScale', bitmapBuilder);
+// fileHandler.readFromFile('non-palette-bitmap.bmp', 'greenScale', bitmapBuilder);
+// fileHandler.readFromFile('non-palette-bitmap.bmp', 'blueScale', bitmapBuilder);
+// fileHandler.readFromFile('non-palette-bitmap.bmp', 'redScale', bitmapBuilder);
+// fileHandler.readFromFile('test-301x301.bmp', 'invert', bitmapBuilder);
+// fileHandler.readFromFile('test-301x301.bmp', 'grayScale', bitmapBuilder);
+// fileHandler.readFromFile('test-301x301.bmp', 'greenScale', bitmapBuilder);
+// fileHandler.readFromFile('test-301x301.bmp', 'blueScale', bitmapBuilder);
+// fileHandler.readFromFile('test-301x301.bmp', 'redScale', bitmapBuilder);
+ fileHandler.readFromFile('cat.jpg', 'invert', bitmapBuilder);
--- bitmap-pocket-protectors/lib/transform.js
+++ bitmap-pocket-protectors/lib/transform.js
@@ -9,8 +9,17 @@ module.exports = function(path, transform, bitmap) {
 };
 
 exports.invert = function(bitmap, callback){
-  console.log(bitmap);
-  for (var i = 0; i < bitmap.colorArray.length; i++) {
+  if(bitmap.colorTable === 0) {
+    let y = 0;
+    while(y <= bitmap.totalSize){
+      let colorValue = bitmap.imageData[y];
+      colorValue = 255 - colorValue;
+      bitmap.imageData[y] = colorValue;
+      y++;
+    }
+    return callback('inverted', bitmap.originalBuffer);
+  }
+  for (let i = 0; i < bitmap.colorArray.length; i++) {
     let colorValue = bitmap.colorArray[i].toString(16);
     colorValue = (255 - colorValue);
     bitmap.colorArray.writeUIntLE(colorValue, i, 1);
@@ -19,39 +28,100 @@ exports.invert = function(bitmap, callback){
 };
 
 exports.grayScale = function(bitmap, callback){
+  if(bitmap.colorTable === 0) {
+    let i = 0;
+    let width = bitmap.lineDataByteWidth;
+    while(i < bitmap.totalSize){
+      let grayscale = ( bitmap.imageData[i] + bitmap.imageData[i + 1] +  bitmap.imageData[i + 2]) / 3;
+      bitmap.imageData[i] = grayscale;
+      bitmap.imageData[i + 1] = grayscale;
+      bitmap.imageData[i + 2] = grayscale;
+      i += 3;
+      if(i === width) {
+        i += bitmap.padding;
+        width += bitmap.fullLineWidth;
+      }
+    }
+    return callback('grayscaled', bitmap.originalBuffer);
+  }
+
   let palette = bitmap.colorArray;
-  console.log(bitmap);
-  for (var i = 0; i < bitmap.colorArray.length; i+=4) {
+  for (let i = 0; i < bitmap.colorArray.length; i+=4) {
     let grayscale = (palette[i] + palette[i + 1] + palette[i + 2]) / 3;
-    bitmap.colorArray.writeUIntLE(grayscale, i, 1);
-    bitmap.colorArray.writeUIntLE(grayscale, i + 1, 1);
-    bitmap.colorArray.writeUIntLE(grayscale, i + 2, 1);
+    palette[i] = grayscale;
+    palette[i + 1] = grayscale;
+    palette[i + 2] = grayscale;
   }
   callback('grayscaled', bitmap.originalBuffer);
 };
 
 exports.greenScale = function(bitmap, callback){
-  console.log(bitmap);
-  for (var i = 0; i < bitmap.colorArray.length; i+=4) {
-    let greenscale = 255;
+  let greenscale = 255;
+  let width = bitmap.lineDataByteWidth;
+  if(bitmap.colorTable === 0) {
+    let i = 0;
+    while(i < bitmap.totalSize){
+      bitmap.imageData[i + 1] = greenscale;
+      i+=3;
+      if(i === width) {
+        i += bitmap.padding;
+        width += bitmap.fullLineWidth;
+      }
+    }
+    return callback('greenscaled', bitmap.originalBuffer);
+  }
+
+  for (let i = 0; i < bitmap.colorArray.length; i+=4) {
     bitmap.colorArray.writeUIntLE(greenscale, i + 1, 1);
   }
   callback('greenscaled', bitmap.originalBuffer);
 };
 
 exports.blueScale = function(bitmap, callback){
-  console.log(bitmap);
-  for (var i = 0; i < bitmap.colorArray.length; i+=4) {
-    let bluescale = 255;
+  let bluescale = 255;
+  let width = bitmap.lineDataByteWidth;
+  if(bitmap.colorTable === 0) {
+    let i = 0;
+    while(i < bitmap.totalSize){
+      bitmap.imageData[i] = bluescale;
+      i+=3;
+      if(i === width) {
+        i += bitmap.padding;
+        width += bitmap.fullLineWidth;
+      }
+    }
+    return callback('bluescaled', bitmap.originalBuffer);
+  }
+  for (let i = 0; i < bitmap.colorArray.length; i+=4) {
     bitmap.colorArray.writeUIntLE(bluescale, i, 1);
   }
   callback('bluescaled', bitmap.originalBuffer);
 };
 
 exports.redScale = function(bitmap, callback){
-  console.log(bitmap);
-  for (var i = 0; i < bitmap.colorArray.length; i+=4) {
-    let redscale = 255;
+  let redscale = 255;
+  let width = bitmap.lineDataByteWidth;
+  if(bitmap.colorTable === 0) {
+    let i = 0;
+    while(i < bitmap.totalSize){
+      bitmap.imageData[i + 2] = redscale;
+      i+=3;
+      if(i === width) {
+        i += bitmap.padding;
+        width += bitmap.fullLineWidth;
+      }
+    }
+    return callback('redscaled', bitmap.originalBuffer);
+  }
+  if(bitmap.colorTable === 0) {
+    let i = 0;
+    while(i < bitmap.totalSize){
+      bitmap.imageData[i + 2] = redscale;
+      i++;
+    }
+    return callback('redscaled', bitmap.originalBuffer);
+  }
+  for (let i = 0; i < bitmap.colorArray.length; i+=4) {
     bitmap.colorArray.writeUIntLE(redscale, i + 2, 1);
   }
   callback('redscaled', bitmap.originalBuffer);
